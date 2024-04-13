36880a909104d937aebb7421cec43918adf17f59
unknown <bariqaanwar34@gmail.com>
2024-04-13 08:42:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what do you want to add in your todos?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "do you want to add more?",
+            default: "true"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
+;
